8711cb2628a887abd68815cdb129ab10d1e87f6c
Armostheus <shubham.chakraboty22@gmail.com>
2019-03-28 21:37:40 +0530

    Added Route to App.js, Created Router links for components, Added new module CreateCompany, Shifted CompanyAdmin and CompanyUser into Companies component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
+import CreateCompany from './components/CreateCompany';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
@@ -15,14 +17,33 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Header 
-          name={this.state.name} 
-          role={this.state.role}
-          logOut={this.logoutUser}
-        />
-        <Body/>
-      </div>
+      <Router>
+        <div className="App">
+          <Header 
+            name={this.state.name} 
+            role={this.state.role}
+            logOut={this.logoutUser}
+          />
+
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+            <h1>Welcome, {this.state.name}</h1>
+            &nbsp;&nbsp;&nbsp;<Link to="/dashboard"><button type="button" class="btn btn-primary">Jump to Dashboard</button></Link>&nbsp;&nbsp;&nbsp;
+            <Link to="/create-company"><button type="button" class="btn btn-primary">Create New Company</button></Link>
+            </React.Fragment>
+          )} />
+          
+
+          <Route path="/dashboard" render={props => (
+            <React.Fragment>
+              <Body/>
+            </React.Fragment>
+          )} />
+
+          <Route path="/create-company" component={CreateCompany} />
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/CreateCompany.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateCompany extends Component {
+    render(){
+        return (
+            <div>               
+                <h1>Here, you will create your company</h1>
+            </div>
+        );
+    }
+}
+
+export default CreateCompany;
